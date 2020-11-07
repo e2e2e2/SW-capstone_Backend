85162791844b22232fd6b01201d67d9951f24612
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-11-07 21:17:10 +0900

    repository format modification

--- src/test/java/com/company/SWcapston_Backend/SwCapstonBackendApplicationTests.java
+++ src/test/java/com/company/SWcapston_Backend/SwCapstonBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cautionisbest.web;
+package com.company.SWcapston_Backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebApplicationTests {
+class SwCapstonBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
