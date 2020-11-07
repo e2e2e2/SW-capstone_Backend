23236c35cd0d24bc68d00b0204b072abbfc94e56
Lee Byoungchan <dinitic@ajou.ac.kr>
2020-11-08 08:06:59 +0900

    add User table interactions

---
+++ src/main/java/com/user/Controller/UserController.java
@@ -0,0 +1,101 @@
+package com.user.Controller;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+
+import com.user.Entity.User;
+import com.user.Entity.tempUser;
+import com.user.Repository.UserRepository;
+import com.user.Service.UserService;
+
+
+	
+@RestController 
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/user")
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE }) 
+	public ResponseEntity<List<User>> getAllmembers() { 
+		List<User> user = userService.findAll(); 
+		return new ResponseEntity<List<User>>(user, HttpStatus.OK); 
+	}
+
+	
+	@RequestMapping("/getUser/{id}")
+    @GetMapping(path = "/getUser/{id}")
+    public User getOneUser(@PathVariable("id") int id) {
+    	System.err.println("add : " + id);
+    	Integer select_id = id;
+    	return userService.findOne(select_id);
+    }
+	
+	@RequestMapping(value="/putUser")
+	@PostMapping()
+	public User saveUser(@RequestBody User user) {
+    	System.err.println(user.getName() + user.getAddress());
+	    return userService.save(user);
+	}
+	
+	@RequestMapping(value="/updateUser/{id}")
+	@PatchMapping(path = "/updateUser/{id}")
+	public User updateUser(@PathVariable("id") int id, @RequestBody tempUser val) {
+		System.err.println("adwdqasaddsdadasdadasdasdasdadasda");
+	    return userService.update(id,val);
+	}
+	
+	
+	@RequestMapping("/delUser/{id}")
+	@DeleteMapping(path = "/delUser/{id}")
+    public int deleteUser(@PathVariable("id") int id) {
+    	System.err.println("delete : " + id);
+    	Integer select_id = id;
+    	return userService.delete(select_id);
+    }
+	
+	
+/*    
+	
+	 // 회원 입력 
+	@PostMapping 
+	public ResponseEntity<User> save(User member) { 
+		return new ResponseEntity<User>(userService.save(member), HttpStatus.OK); 
+	} 
+
+	@RequestMapping(value="/saveMember", method = RequestMethod.GET) 
+	public ResponseEntity<User> save(HttpServletRequest req, User member){ 
+		return new ResponseEntity<User>(userService.save(member), HttpStatus.OK); 
+	}
+	*/
+}
---
+++ src/main/java/com/user/Service/UserService.java
@@ -0,0 +1,75 @@
+package com.user.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.Optional;
+import java.util.Objects;
+
+import com.user.Entity.User;
+import com.user.Entity.tempUser;
+import com.user.Repository.UserRepository;
+	
+@Service("userService")
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+    
+	public List<User> findAll() { 
+		List<User> users = new ArrayList<>();
+		userRepository.findAll().forEach(e -> users.add(e));
+		return users;
+	}
+	 
+	
+	public User findOne(Integer id) {
+		User user = userRepository.getOne(id);
+		System.out.println(user);
+		return user;
+	}
+	
+	 public User save(User user) { 
+		 userRepository.save(user); 
+		 return user; 
+	 }
+	
+	 public int delete(int id) { 
+		 userRepository.deleteById(id); 
+		 return id;
+	 }
+	 
+	 public User update(int id, tempUser val) { 
+		 User user = userRepository.getOne(id);
+		 
+		 
+		 if(val.getAddress() != null)
+			 user.setAddress(val.getAddress());
+		 if(val.getAuth() != -1)
+			 user.setAuth(val.getAuth());
+		 if(val.getName() != null)
+			 user.setName(val.getName());
+		 if(val.getPhone_number() != null)
+			 user.setPhone_number(val.getPhone_number());
+		 if(val.getPassword() != null)
+			 user.setPassword(val.getPassword());
+		 
+		 System.err.println("name : " + user.getName());
+		 userRepository.save(user);
+		 return user; 
+	 }
+	/* 
+	 public Optional<User> findById(Long id) { 
+		 Optional<User> user = userRepository.findById(id);
+		 return user;
+	 }
+	 
+	 
+
+	*/
+		 
+		 
+}
