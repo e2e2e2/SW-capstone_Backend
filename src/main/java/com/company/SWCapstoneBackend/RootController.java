3e447fd28fdb30d8b35ce5fd5b1a68c09532fc6b
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-11-08 17:49:47 +0900

    package format modification

--- src/main/java/com/company/SWCapstoneBackend/RootController.java
+++ src/main/java/com/company/SWCapstoneBackend/RootController.java
@@ -1,4 +1,4 @@
-package com.domain.SWcapstone_Backend;
+package com.company.SWCapstoneBackend;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
