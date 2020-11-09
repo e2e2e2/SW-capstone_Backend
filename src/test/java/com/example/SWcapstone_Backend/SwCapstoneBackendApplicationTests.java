28902fdc139e3b74932b01d5b6128eedf4713d05
luttt <51191255+luttt@users.noreply.github.com>
2020-11-10 01:38:02 +0900

    pass test

--- src/test/java/com/example/SWcapstone_Backend/SwCapstoneBackendApplicationTests.java
+++ src/test/java/com/example/SWcapstone_Backend/SwCapstoneBackendApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.SWcapstone_Backend;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SwCapstoneBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
