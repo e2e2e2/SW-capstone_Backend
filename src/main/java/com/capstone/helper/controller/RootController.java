c5ceef72989e2e2dd9385ef87044005019f11d12
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-11-12 19:39:23 +0900

    Add swagger in build.gradle. And specify RequestMethod in RootController.

--- src/main/java/com/capstone/helper/controller/RootController.java
+++ src/main/java/com/capstone/helper/controller/RootController.java
@@ -1,12 +1,13 @@
 package com.capstone.helper.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
-	@RequestMapping("/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String home() {
 		return "Hello World!";
 	}
