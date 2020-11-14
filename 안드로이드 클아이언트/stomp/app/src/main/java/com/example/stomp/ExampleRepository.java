c277fd40045f546c9c0d83b60e71d2b805879b14
Lee Byoungchan <dinitic@ajou.ac.kr>
2020-11-14 11:27:29 +0900

    add stomp client prototype for android

---
+++ 안드로이드 클아이언트/stomp/app/src/main/java/com/example/stomp/ExampleRepository.java
@@ -0,0 +1,13 @@
+package com.example.stomp;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+
+public interface ExampleRepository {
+
+    @POST("hello-convert-and-send")
+    Completable sendRestEcho(@Query("msg") String message);
+}
