5963075f39db144ce636bb902ad0dd98c4bb272e
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-11-18 19:43:02 +0900

    Recent alarm list request API is implemented.

--- src/main/java/com/capstone/helper/service/AlarmService.java
+++ src/main/java/com/capstone/helper/service/AlarmService.java
@@ -1,5 +1,8 @@
 package com.capstone.helper.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,12 @@ import com.capstone.helper.repository.AlarmRepository;
 public class AlarmService {
 	@Autowired
 	private AlarmRepository alarmRepository;
+	
+	public List<Alarm> findByReceiverId(int receiverId){
+		List<Alarm> alarms = new ArrayList<>();
+		alarmRepository.findByReceiverId(receiverId).forEach(e -> alarms.add(e));
+		return alarms;
+	}
 	
 	public Alarm save(Alarm alarm) { 
 		 alarmRepository.save(alarm); 
