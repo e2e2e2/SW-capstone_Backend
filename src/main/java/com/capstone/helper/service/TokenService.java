27cae5e8ac0edb1b9ef65145af51328bb0eaeed1
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-11-21 03:45:29 +0900

    Temporarily send push alarm to all app device(token).

--- src/main/java/com/capstone/helper/service/TokenService.java
+++ src/main/java/com/capstone/helper/service/TokenService.java
@@ -1,5 +1,7 @@
 package com.capstone.helper.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,7 @@ public class TokenService {
 	@Autowired
 	TokenRepository tokenRepository;
 	
-	public Token findByUserId(int userId) {
+	public List<Token> findByUserId(int userId) {
 		return tokenRepository.findByUserId(userId);
 	}
 	
