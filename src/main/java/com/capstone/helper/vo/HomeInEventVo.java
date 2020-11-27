7f62438cea6b5c2855a4147d2c8f667e291a510b
luttt <51191255+luttt@users.noreply.github.com>
2020-11-27 13:10:53 +0900

    Request Home In Alarm API and Request Home Out Alarm are implemented.

---
+++ src/main/java/com/capstone/helper/vo/HomeInEventVo.java
@@ -0,0 +1,28 @@
+package com.capstone.helper.vo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class HomeInEventVo {
+	
+	private int userId;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private LocalDateTime timestamp;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
+
+}
