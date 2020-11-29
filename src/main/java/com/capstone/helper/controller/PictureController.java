b4e6a901709224210768d04eefa8ec617f1ab127
Lee Byoungchan <dinitic@ajou.ac.kr>
2020-11-30 07:22:29 +0900

    fix picture location

--- src/main/java/com/capstone/helper/controller/PictureController.java
+++ src/main/java/com/capstone/helper/controller/PictureController.java
@@ -1,12 +1,16 @@
 package com.capstone.helper.controller;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +21,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.capstone.helper.model.Picture;
 import com.capstone.helper.service.PictureService;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @RestController
 public class PictureController {
 	
@@ -25,24 +32,28 @@ public class PictureController {
 	
 	//이미지 전송
 	@RequestMapping(value="/test/postImage", method=RequestMethod.POST)
-	public Integer getFallEventByEventId(HttpServletRequest request, @RequestPart MultipartFile data) throws Exception{
+	public Integer getFallEventByEventId(HttpServletRequest request, @RequestPart MultipartFile files) throws Exception{
 		Picture pic = new Picture();
 		
-		String sourceFileName = data.getOriginalFilename(); 
+		String sourceFileName = files.getOriginalFilename(); 
         String sourceFileNameExtension = FilenameUtils.getExtension(sourceFileName).toLowerCase(); 
         File destinationFile; 
         String destinationFileName = FilenameUtils.getBaseName(sourceFileName);
-        String fileUrl = "../Pictures/";
+        String fileUrl = "../../Pictures/";
 
         destinationFile = new File(fileUrl + destinationFileName); 
-        
+        System.out.println("destinationFile = "+ destinationFile);
+        System.out.println("destinationFile.getParentFile() = " + destinationFile.getParentFile());
         destinationFile.getParentFile().mkdirs(); 
-        data.transferTo(destinationFile);
+        files.transferTo(destinationFile.getParentFile());
         
-        pic.setPicURL(fileUrl + destinationFileName);
+        pic.setPicURL(destinationFile + sourceFileNameExtension);
         pic.setEventId(Integer.valueOf(destinationFileName));
         pictureService.save(pic);
         
+        System.out.println("result = "+ pic.getEventId());
         return pic.getEventId();
 	}
+	
+
 }
