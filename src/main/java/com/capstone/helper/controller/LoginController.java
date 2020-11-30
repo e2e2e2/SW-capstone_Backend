c263b1393e30894a62925198eb9725db639b3d85
Lee Byoungchan <dinitic@ajou.ac.kr>
2020-12-01 06:11:08 +0900

    set Error Code for login

--- src/main/java/com/capstone/helper/controller/LoginController.java
+++ src/main/java/com/capstone/helper/controller/LoginController.java
@@ -52,7 +52,7 @@ public class LoginController {
         }
         
         else{
-        	
+        	response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
         	return errerCode;
         }
     }
