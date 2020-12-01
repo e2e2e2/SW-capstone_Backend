e35c2d9746347e8d11464d5e4527833d9de01120
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-12-01 15:33:49 +0900

    Access-Control-Allow-Origin is changed to localhost.

--- src/main/java/com/capstone/helper/config/WebConfig.java
+++ src/main/java/com/capstone/helper/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("http://127.0.0.1");
 	}
 }
