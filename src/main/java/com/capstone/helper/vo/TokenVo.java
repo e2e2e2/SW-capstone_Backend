cddd265e94b711e591623f83a089e570c1fe656c
lut3133 <65217970+lut3133@users.noreply.github.com>
2020-12-10 21:10:09 +0900

    UserId in TokenVo is removed.

--- src/main/java/com/capstone/helper/vo/TokenVo.java
+++ src/main/java/com/capstone/helper/vo/TokenVo.java
@@ -4,23 +4,16 @@ package com.capstone.helper.vo;
 
 public class TokenVo {
 
-	Integer userId;
 	String token;
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
+
 	public String getToken() {
 		return token;
 	}
 	public void setToken(String token) {
 		this.token = token;
 	}
-	public TokenVo(Integer userId, String token) {
+	public TokenVo(String token) {
 		super();
-		this.userId = userId;
 		this.token = token;
 	}
 	public TokenVo() {
